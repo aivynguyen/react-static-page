cd4e8c28c5fec468b4a46ae0a1a20b0cf6d0bba7
aivynguyen <159578635+aivynguyen@users.noreply.github.com>
2025-02-04 22:57:34 -0500

    components

---
+++ components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Content() {
+  return (
+    <p>
+      A speedy hare bragged about how fast he could run.
+      Tired of hearing him boast, the tortoise challenged him to a race...
+      The hare being overconfident, decided to take a nap, and the tortoise moved steadily and eventually won the race.
+    </p>
+  );
+}
+
+export default Content;
---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <p>Moral: Slow and steady wins the race.</p>;
+}
+
+export default Footer;
