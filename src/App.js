9fe6a0845b2b496d3f23c161cc910c4a94472e30
Aivy <aivy@mac.lan>
2025-02-05 00:00:56 -0500

    tortise and hare

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Content() {
+  return (
+    <p>
+      A speedy hare was overconfident about how fast he could run.
+      Tired of hearing him boast, the tortoise challenged him to a race...
+      The hare decided to take a nap while racing, and the tortoise steadily crossed the finish line. 
+      The hare woke up, but it was too late.
+    </p>
+  );
+}
+
+export default Content;
